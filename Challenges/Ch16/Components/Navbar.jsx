b50a259d8433c3f6f37184d3400b5ae189e3f2c2
Tim <timmywilliams4665@gmail.com>
2025-09-26 19:21:52 -0700

    Fixed logo not appearing

--- Challenges/Ch16/Components/Navbar.jsx
+++ Challenges/Ch16/Components/Navbar.jsx
@@ -2,7 +2,7 @@ export default function Navbar() {
     return (
         <header>
             <nav>
-                <img src="/react-logo.png"alt="React-Logo"/>
+                <img src={`${import.meta.env.BASE_URL}react-logo.png`}alt="React-Logo"/>
                 <span>ReactFacts</span>
             </nav>
         </header>
